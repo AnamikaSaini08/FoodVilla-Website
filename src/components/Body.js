e751367c9cdf66cbbd316105130f37dc681be03d
Anamika Saini <86796513+AnamikaSaini08@users.noreply.github.com>
2023-03-24 21:08:22 +0530

    Update Body.js

--- src/components/Body.js
+++ src/components/Body.js
@@ -21,7 +21,7 @@ const Body = ()=>{
 
     async function getRestaurants()
     {
-        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING")
+        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=25.3862404&lng=81.8502118&page_type=DESKTOP_WEB_LISTING")
         const json = await data.json();
         console.log(json);
         //Optional Chaining
